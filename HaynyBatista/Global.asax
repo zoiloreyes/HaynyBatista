dd2c580a3ffe5a8a2756ca5a865ab9f594635941
Zoilo Ismael Reyes Rodríguez <zoiloismaelreyes1@gmail.com>
2018-01-14 10:10:26 -0400

    Add project files.

---
+++ HaynyBatista/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HaynyBatista.MvcApplication" Language="C#" %>
---
+++ HaynyBatista/Scripts/Layout.js
@@ -0,0 +1,21 @@
+﻿
+
+$(document).ready(function () {
+    var LayoutModule = (function () {
+        var InitNav = function () {
+            $("#MenuDivider").css("width", "90%");
+            $('.dropdown-menu').parent().on('show.bs.dropdown', function (e) {
+                $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+            });
+
+            $('.dropdown-menu').parent().on('hide.bs.dropdown', function (e) {
+                $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+            });
+        }
+        return {
+            Start: InitNav
+        }
+    })();
+
+    LayoutModule.Start();
+});
