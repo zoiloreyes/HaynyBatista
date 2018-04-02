119d509a61e7079c44209b5ccab38379701d9d70
zoiloreyes <zoiloismaelreyes1@gmail.com>
2018-04-01 20:26:40 -0400

    Añadido Controlador de Producto

---
+++ HaynyBatista/Controllers/ProductoController.cs
@@ -0,0 +1,19 @@
+﻿using HaynyBatista.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaynyBatista.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Producto
+        public ViewResult List()
+        {
+            return View();
+        }
+    }
+}
